8354718f6287a8a9e983e7dcb89fef705977dbd2
Giuia <gvalentef@gmail.com>
2019-10-21 14:08:11 -0400

    Classe bloco

---
+++ data_types/bloco.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "registro.h"
+
+#ifndef BLOCO_CLASS
+#define BLOCO_CLASS
+
+class Bloco {
+public:
+    Registro registros[2];
+};
+
+#endif
