617c69531d716d31503e6d0a277c57cb2eef1d0c
Giuia <gvalentef@gmail.com>
2019-10-24 23:33:20 -0400

    corta snippet pra 1023 char

--- data_types/registro.h
+++ data_types/registro.h
@@ -14,7 +14,7 @@ public:
     int ano;
     char autores[150];
     int citacoes;
-    char atualizacao[19]; // "XX-XX-XXXX XX:XX:XX"
+    char atualizacao[20]; // "XX-XX-XXXX XX:XX:XX\0"
     char snippet[1024];
 
     Registro(){
@@ -22,38 +22,55 @@ public:
     }
     
     Registro(std::vector< std::string > &tupla){
-        size_t pos = 0;
-
         // setVazio
         vazio = false;
 
         // setId
         if(!null_atribute(tupla.at(0)))
             id = stoi(tupla.at(0));
+        else
+            id = 0;
 
         // setTitulo
         if(!null_atribute(tupla.at(1)))
             copy_string_to_char(tupla.at(1), titulo);
+        else
+            copy_string_to_char("", titulo);
 
         // setAno
         if(!null_atribute(tupla.at(2)))
             ano = stoi(tupla.at(2));
+        else
+            ano = 0;
 
         // setAutores
         if(!null_atribute(tupla.at(3)))
             copy_string_to_char(tupla.at(3), autores);
+        else 
+            copy_string_to_char("", autores);
         
         // setCitacoes
         if(!null_atribute(tupla.at(4)))
             citacoes = stoi(tupla.at(4));
+        else 
+            citacoes = 0;
+        
         
         // setAtualizacao
         if(!null_atribute(tupla.at(5)))
             copy_string_to_char(tupla.at(5), atualizacao);
+        else
+            copy_string_to_char("", atualizacao);
         
+        if(tupla.at(6).length() > 1023){
+            tupla.at(6) = tupla.at(6).substr(0, 1022);
+        }
+
         // setSnippet
         if(!null_atribute(tupla.at(6)))
             copy_string_to_char(tupla.at(6), snippet);
+        else
+            copy_string_to_char("", snippet);
     }
 
     void copy_string_to_char(std::string str, char * charArray){
