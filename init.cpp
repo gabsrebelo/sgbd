cfe6cb3c82d372b2bb0b5e8467d41a179d275a62
Giuia <gvalentef@gmail.com>
2019-10-25 20:42:31 -0400

    ajusta init

--- init.cpp
+++ init.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <stdio.h>
 #include "helpers/hash.h"
+#include "helpers/parser.h"
 
 int main(){
     init_hash_file();
-    
+    parse_file();
     
     return 0;
 }
